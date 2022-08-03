bfcd2dfd829816ae04f13883f1e6cc30c50df407
Tamaized <9671313+Tamaized@users.noreply.github.com>
2022-08-03 12:40:32 -0400

    Update Forge to 1.19.1

--- src/main/java/tamaized/regutil/RegUtil.java
+++ src/main/java/tamaized/regutil/RegUtil.java
@@ -172,7 +172,6 @@ public class RegUtil {
 			register.register(bus);
 	}
 
-	@SuppressWarnings("unchecked")
 	public static <R> DeferredRegister<R> create(IForgeRegistry<R> type) {
 		return create(type.getRegistryKey());
 	}
