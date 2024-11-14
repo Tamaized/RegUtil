02c1e84e948b444c80c933659eaad38c62d55e03
Tamaized <9671313+Tamaized@users.noreply.github.com>
2024-11-14 00:25:47 -0500

    Actually fix it

--- src/main/java/tamaized/regutil/AttributeFactory.java
+++ src/main/java/tamaized/regutil/AttributeFactory.java
@@ -5,7 +5,8 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
@@ -13,8 +14,12 @@ import java.util.stream.Stream;
 public interface AttributeFactory extends Function<ItemStack, Stream<ItemAttributeModifiers.Entry>> {
 
 	@SafeVarargs
+	@SuppressWarnings({"UseBulkOperation", "ManualArrayToCollectionCopy"})
 	static AttributeFactory make(Supplier<AttributeData>... data) {
-		return stack -> Arrays.stream(data)
+		List<Supplier<AttributeData>> list = new ArrayList<>();
+		for (Supplier<AttributeData> o : data)
+			list.add(o);
+		return stack -> list.stream()
 			.map(Supplier::get)
 			.filter(a -> a.test().test(stack))
 			.map(d -> new ItemAttributeModifiers.Entry(d.attribute(), new AttributeModifier(ResourceLocation.fromNamespaceAndPath(RegUtil.getModID(), d.id()), d.value(), d.op()), d.slot()));
