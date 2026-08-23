c554a51920628d0c35fe2a9edba2e5c9e21d3d32
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-08-23 08:17:47 -0400

    Confirmed that this isn't needed anymore, vanilla supports this out of the box finally

--- src/main/java/tamaized/regutil/RegUtil.java
+++ src/main/java/tamaized/regutil/RegUtil.java
@@ -32,59 +32,6 @@ public class RegUtil {
 //		brokenStateName = Identifier.fromNamespaceAndPath(modIdProvider.getModId().orElseThrow(), "broken_state_attributes").toString();
 		@NotNull IEventBus bus = Objects.requireNonNull(ModLoadingContext.get().getActiveContainer().getEventBus());
 		registers.create(Registries.ITEM); // Pre-Bake the Item DeferredRegister for ToolAndArmorHelper
-		// Looks like smithing templates merge data now instead of overwrite, TODO: double check this behavior
-		/*class FixedUpgradeRecipe extends SmithingTransformRecipe {
-			final Ingredient template;
-			final Ingredient base;
-			final Ingredient addition;
-			final ItemStack result;
-			public FixedUpgradeRecipe(Ingredient pTemplate, Ingredient pBase, Ingredient pAddition, ItemStack pResult) {
-				super(pTemplate, pBase, pAddition, pResult);
-				template = pTemplate;
-				base = pBase;
-				addition = pAddition;
-				result = pResult;
-			}
-
-			@Override
-			public ItemStack assemble(SmithingRecipeInput input, HolderLookup.Provider registries) {
-				ItemStack itemstack = input.base().transmuteCopy(this.result.getItem(), this.result.getCount());
-				itemstack.applyComponents(this.result.getComponentsPatch());
-				return itemstack;
-
-				CompoundTag compoundtag = pContainer.getItem(1).getTag();
-				if (compoundtag != null)
-					itemstack.getOrCreateTag().merge(compoundtag.copy());
-				return itemstack;
-			}
-		}
-		create(Registries.RECIPE_SERIALIZER).register("smithing", () -> new SmithingTransformRecipe.Serializer() {
-			private static final Codec<SmithingTransformRecipe> CODEC = RecordCodecBuilder.create(
-					p_301062_ -> p_301062_.group(
-									Ingredient.CODEC.fieldOf("template").forGetter(p_301310_ -> ((FixedUpgradeRecipe) p_301310_).template),
-									Ingredient.CODEC.fieldOf("base").forGetter(p_300938_ -> ((FixedUpgradeRecipe) p_300938_).base),
-									Ingredient.CODEC.fieldOf("addition").forGetter(p_301153_ -> ((FixedUpgradeRecipe) p_301153_).addition),
-									ItemStack.ITEM_WITH_COUNT_CODEC.fieldOf("result").forGetter(p_300935_ -> ((FixedUpgradeRecipe) p_300935_).result)
-							)
-							.apply(p_301062_, (template, base, addition, result) -> (SmithingTransformRecipe) new FixedUpgradeRecipe(template, base, addition, result))
-			);
-
-			@Override
-			public Codec<SmithingTransformRecipe> codec() {
-				return CODEC;
-			}
-
-			@Override
-			public SmithingTransformRecipe fromNetwork(FriendlyByteBuf p_267316_) {
-				Ingredient ingredient = Ingredient.fromNetwork(p_267316_);
-				Ingredient ingredient1 = Ingredient.fromNetwork(p_267316_);
-				Ingredient ingredient2 = Ingredient.fromNetwork(p_267316_);
-				ItemStack itemstack = p_267316_.readItem();
-				return new FixedUpgradeRecipe(ingredient, ingredient1, ingredient2, itemstack);
-			}
-
-		});*/
-
 		registers.getRegisters().forEach(register -> register.register(bus));
 	}
 
